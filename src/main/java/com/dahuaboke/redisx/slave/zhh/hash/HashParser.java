71db1fdc99b5e2b99caf819dea7a1908dc9c66e6
zhh的R9000 <15536508342@163.com>
2024-05-21 10:14:57 +0800

    添加 hashParser

---
+++ src/main/java/com/dahuaboke/redisx/slave/zhh/hash/HashParser.java
@@ -0,0 +1,29 @@
+package com.dahuaboke.redisx.slave.zhh.hash;
+
+import com.dahuaboke.redisx.slave.zhh.LengthParser;
+import com.dahuaboke.redisx.slave.zhh.StringParser;
+import io.netty.buffer.ByteBuf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Desc:
+ * @Author：zhh
+ * @Date：2024/5/21 9:23
+ */
+public class HashParser {
+    LengthParser length = new LengthParser();
+    StringParser string = new StringParser();
+
+    public Map<byte[],byte[]> parseHash(ByteBuf byteBuf){
+        long len = length.parseLength(byteBuf).len;
+        Map<byte[],byte[]> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            byte[] key = string.parseString(byteBuf);
+            byte[] value = string.parseString(byteBuf);
+            map.put(key,value);
+        }
+        return map;
+    }
+}
