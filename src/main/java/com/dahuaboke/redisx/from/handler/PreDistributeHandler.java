cc2d328ef49fc1213a5f12dcfc74c5be9cdcd24f
changdongliang0121 <2319533864@qq.com>
2024-06-12 16:41:47 +0800

    redis指令分发操作优化

--- src/main/java/com/dahuaboke/redisx/from/handler/PreDistributeHandler.java
+++ src/main/java/com/dahuaboke/redisx/from/handler/PreDistributeHandler.java
@@ -22,8 +22,6 @@ public class PreDistributeHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(PreDistributeHandler.class);
 
-    private boolean lineBreakFlag = true;
-
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         ctx.channel().attr(Constant.RDB_STREAM_NEXT).set(false);
@@ -34,56 +32,45 @@ public class PreDistributeHandler extends ChannelInboundHandlerAdapter {
         if (msg instanceof ByteBuf) {
             ByteBuf in = (ByteBuf) msg;
             StringBuilder sb = new StringBuilder();
-            sb.append("\r\n<").append(Thread.currentThread().getName()).append(">")
+            sb.append("\r\n")
+                    .append("<").append(Thread.currentThread().getName()).append(">")
                     .append("<redis massage> = ").append(in).append("\r\n");
             sb.append(ByteBufUtil.prettyHexDump(in).toString());
-            logger.debug(sb.toString());
+            logger.trace(sb.toString());
 
             if (ctx.pipeline().get(Constant.INIT_SYNC_HANDLER_NAME) != null) {
                 ctx.fireChannelRead(in);
             } else if (ctx.channel().attr(Constant.RDB_STREAM_NEXT).get()){
                 logger.debug("Receive rdb byteStream length [{}]", in.readableBytes());
                 ctx.fireChannelRead(new RdbCommand(in));
-            } else{//redis指令流程
+            } else{
                 //redis 7.X版本会发空字符串后在fullresync
-                if (lineBreakFlag && in.getByte(0) == '\n') {
-                    //redis 7.X版本会发空字符串后在fullresync
-                    while(in.getByte(in.readerIndex()) == Constant.LINE_BREAK){
-                        in.readByte();
-                        if(in.readerIndex() == in.writerIndex()){
-                            in.release();
-                            return;
-                        }
+                while(in.getByte(in.readerIndex()) == Constant.LINE_BREAK){
+                    in.readByte();
+                    if(in.readerIndex() == in.writerIndex()){
+                        in.release();
+                        return;
                     }
                 }
-                lineBreakFlag = false;
-                switch (in.getByte(in.readerIndex())) {
-                    case Constant.PLUS:// + 开头
-                        String headStr = in.readBytes(ByteBufUtil.indexOf(Constant.SEPARAPOR, in) - in.readerIndex()).toString(StandardCharsets.UTF_8);
-                        if (Constant.CONTINUE.equals(headStr)) {
-                            logger.debug("Find continue command do nothing");
-                            in.release();
-                        } else if (headStr.startsWith(Constant.CONTINUE)) {
-                            logger.debug("Find continue command and will reset offset");
-                            ctx.fireChannelRead(new OffsetCommand(headStr));
-                            in.release();
-                        } else if (headStr.startsWith(Constant.FULLRESYNC)) {
-                            logger.debug("Find fullReSync command");
-                            ctx.fireChannelRead(new OffsetCommand(headStr));
-                            ctx.channel().attr(Constant.RDB_STREAM_NEXT).set(true);
-                            in.release();
-                        } else {
-                            ctx.fireChannelRead(in);
-                        }
-                        break;
-                    case Constant.MINUS:// - 开头,错误信息
-                        in.release();
-                        logger.error("redis error message [{}]!", in.toString(CharsetUtil.UTF_8));
-                        break;
-                    case Constant.STAR:// * 开头，命令信息
-                    case Constant.DOLLAR:
-                    default://其他种类开头
+                if(in.getByte(in.readerIndex()) == Constant.PLUS){
+                    String headStr = in.readBytes(ByteBufUtil.indexOf(Constant.SEPARAPOR, in) - in.readerIndex()).toString(StandardCharsets.UTF_8);
+                    in.readBytes(Constant.SEPARAPOR.readableBytes());
+                    if (Constant.CONTINUE.equals(headStr)) {
+                        logger.debug("Find continue command do nothing");
+                    } else if (headStr.startsWith(Constant.CONTINUE)) {
+                        logger.debug("Find continue command and will reset offset");
+                        ctx.fireChannelRead(new OffsetCommand(headStr));
+                    } else if (headStr.startsWith(Constant.FULLRESYNC)) {
+                        logger.debug("Find fullReSync command");
+                        ctx.fireChannelRead(new OffsetCommand(headStr));
+                        ctx.channel().attr(Constant.RDB_STREAM_NEXT).set(true);
+                    }else{
+                        in.readerIndex(0);
                         ctx.fireChannelRead(in);
+                    }
+                }else{
+                    in.readerIndex(0);
+                    ctx.fireChannelRead(in);
                 }
             }
 
